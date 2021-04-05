664149d665ede0cd5fb776b555fd69cb79c61751
Shouvick <deeprony7@gmail.com>
2021-04-05 23:54:19 +0530

    named vs default exports

---
+++ songs/src/actions/index.tsx
@@ -0,0 +1,9 @@
+// Action creater
+
+export const selectSong = (song: string) => {
+    // Return an action
+    return {
+        type: 'SONG_SELECTED',
+        payload: song
+    }
+}
--- songs/src/components/App.tsx
+++ songs/src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import { selectSong } from '../actions'
 
-
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h2>Shouvick</h2>
