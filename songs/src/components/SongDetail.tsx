fbf6c1393e2c71e9b173c6c24026fb8b749e0bbd
Shouvick <deeprony7@gmail.com>
2021-04-06 23:11:54 +0530

    condtional rendering

--- songs/src/components/SongDetail.tsx
+++ songs/src/components/SongDetail.tsx
@@ -1,17 +1,30 @@
 import React from 'react'
 import { connect } from 'react-redux';
 
-const SongDetail = (props: any) => {
-    console.log(props);
+interface SongProp {
+    title: string,
+    duration: string
+}
+
+const SongDetail = ({ song }: { song: SongProp }) => {
+    if (!song) {
+        return <div>Select a song</div>
+    }
+
     return (
         <div>
-            SongDetail
+            <h3>Details for:</h3>
+            <p>
+                Title: {song.title}
+                <br/>
+                Duration: {song.duration}
+            </p>
         </div>
     )
 }
 
 const mapStateToProps = (state: any) => {
-    return { song: state.selectedSong}
+    return { song: state.selectedSong }
 }
 
 export default connect(mapStateToProps)(SongDetail)
