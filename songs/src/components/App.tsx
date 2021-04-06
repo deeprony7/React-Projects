cc7fd7c69ae0026b44c1e31edb278117b1fe1e06
Shouvick <deeprony7@gmail.com>
2021-04-07 00:44:34 +0530

    added material navbar

--- songs/src/components/App.tsx
+++ songs/src/components/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import SongList from './SongList'
 import SongDetail from './SongDetail'
+import Navbar from './Navbar'
 
 const App = () => {
   return (
+    <React.Fragment>
+    <Navbar/>
     <div className="ui container grid">
       <div className="ui row">
         <div className="column eight wide">
@@ -14,6 +17,7 @@ const App = () => {
         </div>
       </div>
     </div>
+    </React.Fragment>
   );
 }
 
