bee6bcb4daed340ac400a9dcf9f50ba17bb2ada3
Shouvick <deeprony7@gmail.com>
2021-04-10 20:46:28 +0530

    initial commit

--- songs/src/components/App.tsx
+++ songs/src/components/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import SongList from './SongList'
 import SongDetail from './SongDetail'
 import Navbar from './Navbar'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 const App = () => {
   return (
     <React.Fragment>
       <Navbar/>
+      <TextField />
       <br/>
       <div className="ui container grid">
         <div className="ui row">
