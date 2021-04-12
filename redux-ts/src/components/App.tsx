cf6b8ac51c7049efa098300e477ae8d229521d28
Shouvick <deeprony7@gmail.com>
2021-04-13 00:30:44 +0530

    wired up react

--- redux-ts/src/components/App.tsx
+++ redux-ts/src/components/App.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import { Provider } from "react-redux";
+import { store } from "../state";
 
 const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
-}
+  return (
+      <Provider store={store}>
+          <div>
+              <h1>Search for a Package</h1>
+              <RepositoriesList/>
+          </div>
+      </Provider>
+  )
+};
 
-export default App
+export default App;
