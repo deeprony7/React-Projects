4894b3e4d3ec76a18913f95e98721ec8f34d8c4d
Shouvick <deeprony7@gmail.com>
2021-04-14 00:49:38 +0530

    fetch singuler user

--- blog/src/actions/index.tsx
+++ blog/src/actions/index.tsx
@@ -1,15 +1,23 @@
-import { ThunkDispatch } from 'redux-thunk';
-import jsonPlaceholder from '../apis/jsonPlaceholder'
-import { Action } from 'redux';
+import { ThunkDispatch } from "redux-thunk";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+import { Action } from "redux";
 
 type MyRootState = {};
 type MyExtraArg = undefined;
 type MyThunkDispatchProp = ThunkDispatch<MyRootState, MyExtraArg, Action>;
 
 export const fetchPosts = () => {
-    return async (dispatch: MyThunkDispatchProp) => {
-        const response = await jsonPlaceholder.get('/posts')
+  return async (dispatch: MyThunkDispatchProp) => {
+    const response = await jsonPlaceholder.get("/posts");
 
-        dispatch({ type: 'FETCH_POSTS', payload: response.data })
-    }
-}
+    dispatch({ type: "FETCH_POSTS", payload: response.data });
+  };
+};
+
+export const fetchUser = (id: number) => {
+  return async (dispatch: MyThunkDispatchProp) => {
+    const response = await jsonPlaceholder.get(`./users/${id}`);
+
+    dispatch({ type: "FETCH_USER", payload: response.data });
+  };
+};
