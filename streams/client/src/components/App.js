d246f88550575bd6e38b6783227934312c61e917
Shouvick <deeprony7@gmail.com>
2021-05-17 20:33:27 +0530

    component scaffolding

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+};
+
+const PageTwo = () => {
+  return (
+    <div>
+      PageTwo
+      <Link to="/">
+        <button>Click me!</button>
+      </Link>
+    </div>
+  );
+};
 
 const App = () => {
-    return (
-        <div>
-            App
-        </div>
-    )
-}
+  return (
+    <div>
+      <Router>
+        <Route path="/" exact component={PageOne}></Route>
+        <Route path="/pagetwo" component={PageTwo}></Route>
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
