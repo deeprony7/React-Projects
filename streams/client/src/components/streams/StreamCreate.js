336ca2bd8fcd3684411837f38be4a2de3defec32
Shouvick <deeprony7@gmail.com>
2021-05-27 23:34:39 +0530

    display validation msgs

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 const StreamCreate = (props) => {
-  const renderInput = ({ input, label }) => {
+  const renderInput = ({ input, label, meta }) => {
     return (
       <div className="field">
         <label>{label}</label>
         <input {...input} />
+        <div>{meta.error}</div>
       </div>
     );
   };
@@ -28,6 +29,21 @@ const StreamCreate = (props) => {
   );
 };
 
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
 export default reduxForm({
   form: "streamCreate",
+  validate,
 })(StreamCreate);
