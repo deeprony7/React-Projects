4e306f192ab69be22978405521ccbd925a2e82e3
Shouvick <deeprony7@gmail.com>
2021-05-31 23:20:11 +0530

    bugfix: stream create input box lost focus on typing. It was caused due to components of renderInput and renderError being inside the functional component. Hence moving them out of the component fixed the problem.

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -3,28 +3,42 @@ import { Field, reduxForm } from "redux-form";
 import { connect } from "react-redux";
 import { createStream } from "../../actions";
 
-const StreamCreate = (props) => {
-  const renderError = ({ error, touched }) => {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  };
+const renderInput = ({ input, label, meta }) => {
+  const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+  return (
+    <div className={className}>
+      <label>{label}</label>
+      <input {...input} autoComplete="off" />
+      {renderError(meta)}
+    </div>
+  );
+};
 
-  const renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
+const renderError = ({ error, touched }) => {
+  if (touched && error) {
     return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {renderError(meta)}
+      <div className="ui error message">
+        <div className="header">{error}</div>
       </div>
     );
-  };
+  }
+};
 
+const validate = (formValues) => {
+  const errors = {};
+
+  if (!formValues.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
+const StreamCreate = (props) => {
   const onSubmit = (formValues) => {
     props.createStream(formValues);
   };
@@ -40,20 +54,6 @@ const StreamCreate = (props) => {
       <button className="ui button primary">Submit</button>
     </form>
   );
-};
-
-const validate = (formValues) => {
-  const errors = {};
-
-  if (!formValues.title) {
-    errors.title = "You must enter a title";
-  }
-
-  if (!formValues.description) {
-    errors.description = "You must enter a description";
-  }
-
-  return errors;
 };
 
 const formWrapped = reduxForm({
