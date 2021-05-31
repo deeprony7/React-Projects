9bb246ef238c48edde921164a8ceb77d67633038
Shouvick <deeprony7@gmail.com>
2021-05-31 23:04:04 +0530

    rendereing all streams

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -8,7 +8,30 @@ const StreamList = (props) => {
     props.fetchStreams();
   }, []);
 
-  return <div>StreamList</div>;
+  const renderList = () => {
+    return props.streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h2>Streams</h2>
+      <div className="ui celled list">{renderList()}</div>
+    </div>
+  );
 };
 
-export default connect(null, { fetchStreams })(StreamList);
+const mapStateToProps = (state) => {
+  return { streams: Object.values(state.streams) };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
