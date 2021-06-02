61d2fc35e66ec663f13590f2cff4bb48f2d9cc7d
Shouvick <deeprony7@gmail.com>
2021-06-02 19:48:30 +0530

    wildcard navigation

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -13,7 +13,9 @@ const StreamList = (props) => {
     if (stream.userId === props.CurrentUserId) {
       return (
         <div className="right floated content">
-          <button className="ui button primary">Edit</button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
           <button className="ui button negative">Delete</button>
         </div>
       );
