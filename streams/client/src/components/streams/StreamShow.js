7c18bcb6bb1544ba2af977aa69442c1878761744
Shouvick <deeprony7@gmail.com>
2021-06-09 00:05:51 +0530

    hooked up steam show with title and description

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamShow = () => {
-    return (
-        <div>
-            StreamShow
-        </div>
-    )
-}
+const StreamShow = ({ fetchStream, match, stream }) => {
+  useEffect(() => {
+    fetchStream(match.params.id);
+  }, []);
 
-export default StreamShow
+  if (!stream) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>{stream.title}</h1>
+      <h5>{stream.description}</h5>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
