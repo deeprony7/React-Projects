e6a9fb6fb830157ac530fbb592497988dab4f48f
Shouvick <deeprony7@gmail.com>
2021-06-11 00:29:09 +0530

    Added context

--- translate-context/src/components/App.js
+++ translate-context/src/components/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import UserCreate from './UserCreate';
-
+import React from "react";
+import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
 class App extends React.Component {
-  state = { language: 'english' };
+  state = { language: "english" };
 
-  onLanguageChange = language => {
+  onLanguageChange = (language) => {
     this.setState({ language });
   };
 
@@ -15,14 +15,16 @@ class App extends React.Component {
           Select a language:
           <i
             className="flag us"
-            onClick={() => this.onLanguageChange('english')}
+            onClick={() => this.onLanguageChange("english")}
           />
           <i
             className="flag nl"
-            onClick={() => this.onLanguageChange('dutch')}
+            onClick={() => this.onLanguageChange("dutch")}
           />
         </div>
-        <UserCreate />
+        <LanguageContext.Provider value={this.state.language}>
+          <UserCreate />
+        </LanguageContext.Provider>
       </div>
     );
   }
